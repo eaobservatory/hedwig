9b4c877024883a3018b9593ca61b0cc9a659e2b9
Graham Bell <g.bell@eaobservatory.org>
2015-06-05 14:59:33 -1000

    jcmt: Add web interface for observing requests

---
+++ data/web/static/js/edit_jcmt_request.js
@@ -0,0 +1,31 @@
+function addRequestRow(newRowNumber) {
+    var nid = 'new_' + newRowNumber;
+    var newrow = $('#requestrow_template').clone();
+    newrow.attr('id', 'requestrow_' + nid);
+    newrow.find('[name=instrument]').attr('name', 'instrument_' + nid);
+    newrow.find('[name=weather]').attr('name', 'weather_' + nid);
+    newrow.find('[name=time]').attr('name', 'time_' + nid);
+    var deletelink = newrow.find('#delete_template');
+    deletelink.attr('id', 'delete_' + nid);
+    deletelink.click(function (event) {
+        deleteRequestRow(nid);
+    });
+    newrow.appendTo($('table#requests'));
+}
+
+function deleteRequestRow(requestid) {
+    $('#requestrow_' + requestid).remove();
+}
+
+$(document).ready(function () {
+    var newRowCounter = 1;
+
+    $('#add_request').click(function (event) {
+        addRequestRow(newRowCounter ++);
+    });
+
+    $('[id^="delete_"]').click(function (event) {
+        var requestid = event.target.id.replace('delete_', '');
+        deleteRequestRow(requestid);
+    });
+});
--- lib/insertnamehere/facility/jcmt/view.py
+++ lib/insertnamehere/facility/jcmt/view.py
@@ -18,7 +18,12 @@
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
+from ...error import UserError
+from ...web.util import HTTPRedirect, flash, url_for
+from ...view.util import organise_collection, with_proposal
 from ..generic.view import Generic
+from .type import JCMTInstrument, JCMTRequest, JCMTRequestCollection, \
+    JCMTWeather
 
 
 class JCMT(Generic):
@@ -33,3 +38,79 @@ class JCMT(Generic):
         return 'M{0}{1}{2:03d}'.format(
             proposal.semester_code, proposal.queue_code, proposal.number
         ).upper()
+
+    def _view_proposal_extra(self, db, proposal):
+        ctx = super(JCMT, self)._view_proposal_extra(db, proposal)
+
+        requests = db.search_jcmt_request(proposal_id=proposal.id)
+
+        # TODO: organise results sensibly rather than just outputting them
+        # as recieved from the database.
+        ctx.update({
+            'requests': [x._replace(
+                instrument=JCMTInstrument.get_name(x.instrument),
+                weather=JCMTWeather.get_name(x.weather),
+            ) for x in requests.values()],
+        })
+
+        return ctx
+
+    @with_proposal(permission='edit')
+    def view_request_edit(self, db, proposal, can, form, is_post):
+        message = None
+
+        records = db.search_jcmt_request(proposal_id=proposal.id)
+
+        if is_post:
+            # Temporary dictionaries for new records.
+            updated_records = {}
+            added_records = {}
+
+            for param in form:
+                if not param.startswith('time_'):
+                    continue
+                id_ = param[5:]
+
+                if id_.startswith('new_'):
+                    request_id = int(id_[4:])
+                    destination = added_records
+                else:
+                    request_id = int(id_)
+                    destination = updated_records
+
+                request_time = form[param]
+                try:
+                    request_time = float(request_time)
+                except ValueError:
+                    # Ignore parsing error for now so that we can leave
+                    # whatever the user typed in to form for them to correct.
+                    pass
+
+                destination[request_id] = JCMTRequest(
+                    request_id, proposal.id,
+                    int(form['instrument_' + id_]),
+                    int(form['weather_' + id_]),
+                    request_time)
+
+            records = organise_collection(JCMTRequestCollection,
+                                          updated_records, added_records)
+
+            try:
+                db.sync_jcmt_proposal_request(proposal.id, records)
+
+                flash('The observing request has been saved.')
+
+                raise HTTPRedirect(url_for('.proposal_view',
+                                           proposal_id=proposal.id))
+
+            except UserError as e:
+                message = e.message
+
+        return {
+            'title': 'Edit Observing Request',
+            'message': message,
+            'proposal_id': proposal.id,
+            'requests': records.values(),
+            'instruments': JCMTInstrument.get_options(),
+            'weathers': JCMTWeather.get_options(),
+        }
