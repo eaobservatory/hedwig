c9fbd883a8e363f921e21a11902bd300a35ba2f9
Graham Bell <g.bell@eaobservatory.org>
2015-07-30 14:41:48 -1000

    help: Add navigation links to help pages

--- lib/hedwig/view/help.py
+++ lib/hedwig/view/help.py
@@ -23,11 +23,13 @@ import os.path
 import re
 
 from ..web.format import format_text_rst
-from ..web.util import HTTPError, HTTPNotFound
+from ..web.util import HTTPError, HTTPNotFound, url_for
 
 
 valid_page_name = re.compile('^([-_a-z0-9]+)$')
 
+NavLink = namedtuple(
+    'NavLink', ('up', 'up_title', 'prev', 'prev_title', 'next', 'next_title'))
 
 TOCEntry = namedtuple('TOCEntry', ('mtime', 'title'))
 
@@ -52,6 +54,32 @@ def prepare_help_page(doc_root, page_name, toc_cache):
 
         file_name = m.group(1)
 
+    (body, title, toc) = _read_rst_file(doc_root, file_name)
+
+    toc_entries = _process_toc_entries(doc_root, toc, toc_cache)
+
+    if page_name is None:
+        nav_link = NavLink(url_for('.help_index'), 'Help',
+                           None, None, None, None)
+
+    else:
+        nav_link = _find_nav_link(doc_root, page_name, 'index', toc_cache)
+
+    return {
+        'title': title,
+        'help_text': body,
+        'toc': toc_entries,
+        'nav_link': nav_link,
+    }
+
+
+def _read_rst_file(doc_root, file_name):
+    """
+    Read a file and return the body, title and toc.
+
+    Raises HTTPNotFound if the file doesn't exist.
+    """
+
     path_name = os.path.join(doc_root, file_name + '.rst')
 
     if not os.path.exists(path_name):
@@ -60,8 +88,16 @@ def prepare_help_page(doc_root, page_name, toc_cache):
     with open(os.path.join(path_name)) as f:
         text = f.read()
 
-    (body, title, toc) = format_text_rst(text, extract_title_toc=True,
-                                         start_heading=2)
+    return format_text_rst(text, extract_title_toc=True, start_heading=2)
+
+
+def _process_toc_entries(doc_root, toc, toc_cache):
+    """
+    Processes the TOC obtained from an RST file and returns
+    an OrderedDict of entries and their titles.
+
+    Also takes a reference to the TOC cache dictionary.
+    """
 
     toc_entries = OrderedDict()
 
@@ -85,8 +121,50 @@ def prepare_help_page(doc_root, page_name, toc_cache):
 
         toc_entries[toc_entry] = toc_entry_title
 
-    return {
-        'title': title,
-        'help_text': body,
-        'toc': toc_entries,
-    }
+    return toc_entries
+
+
+def _find_nav_link(doc_root, target_name, page_name, toc_cache):
+    """
+    """
+
+    toc_i = None
+
+    try:
+        (body, title, toc) = _read_rst_file(doc_root, page_name)
+
+        toc_i = toc.index(target_name)
+
+    except HTTPNotFound:
+        return None
+
+    except ValueError:
+        pass
+
+    if toc_i is not None:
+        # The target page was in this TOC.  Create the NavLink object.
+        toc_entries = _process_toc_entries(doc_root, toc, toc_cache)
+
+        nl = NavLink(('./' if page_name == 'index' else page_name),
+                     title, None, None, None, None)
+
+        if toc_i > 0:
+            prev = toc[toc_i - 1]
+            nl = nl._replace(prev=prev, prev_title=toc_entries[prev])
+
+        if toc_i < (len(toc) - 1):
+            next_ = toc[toc_i + 1]
+            nl = nl._replace(next=next_, next_title=toc_entries[next_])
+
+        return nl
+
+    else:
+        # Recurse through the TOC entries looking for a match.
+        for toc_entry in toc:
+            ans = _find_nav_link(doc_root, target_name, toc_entry, toc_cache)
+
+            if ans is not None:
+                return ans
+
+    # If we didn't find anything, return None.
+    return None
