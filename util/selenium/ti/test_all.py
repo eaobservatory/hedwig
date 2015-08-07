b55dbbe71853b188247a27c166912ee7594ac3e2
Graham Bell <g.bell@eaobservatory.org>
2015-08-06 17:38:43 -1000

    util: Outline status in the complete screenshot

--- util/selenium/ti/test_all.py
+++ util/selenium/ti/test_all.py
@@ -809,7 +809,8 @@ class IntegrationTest(DummyConfigTestCase):
         self.browser.find_element_by_link_text('Back to proposal').click()
 
         # Grab a screenshot of the complete proposal now.
-        self._save_screenshot(self.user_image_root, 'proposal_complete')
+        self._save_screenshot(self.user_image_root, 'proposal_complete',
+                              ['proposal_status_cell'])
 
         self.browser.find_element_by_link_text('Withdraw proposal').click()
 
