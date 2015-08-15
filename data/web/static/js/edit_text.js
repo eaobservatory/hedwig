fcb218dc88ccc00b36ec1e9639cc947c42e2b379
Graham Bell <g.bell@eaobservatory.org>
2015-08-15 13:48:12 -1000

    web: Use closure for JavaScript word count func.
    Now define this inside the document ready function so that it can
    refer to variables defined in its scope.  Then we don't need to
    create the jQuery selectors multiple times.

--- data/web/static/js/edit_text.js
+++ data/web/static/js/edit_text.js
@@ -1,17 +1,19 @@
-function makeWordCount() {
-    var wordArea = $('[name="text"]');
-    var words = wordArea.val().split(/\s+/).length;
-    var wordLimit = parseInt(wordArea.data('wordlimit'), 10);
-    var wordCount = $('#text_words');
-    wordCount.text(words + ' / ' + wordLimit + ' words');
-    if (words > wordLimit) {
-        wordCount.removeClass('forminfo_ok').addClass('forminfo_bad');
-    } else {
-        wordCount.removeClass('forminfo_bad').addClass('forminfo_ok');
-    }
-}
-
 $(document).ready(function () {
+    var wordArea = $('[name="text"]').first();
+    var wordLimit = parseInt(wordArea.data('wordlimit'), 10);
+    var wordCount = $('#text_words').first();
+
+    var makeWordCount = (function () {
+        var words = wordArea.val().split(/\s+/).length;
+        wordCount.text(words + ' / ' + wordLimit + ' words');
+        if (words > wordLimit) {
+            wordCount.removeClass('forminfo_ok').addClass('forminfo_bad');
+        } else {
+            wordCount.removeClass('forminfo_bad').addClass('forminfo_ok');
+        }
+    });
+
     makeWordCount();
+
     $('[name="text"]').on('input', makeWordCount);
 });
