a461e0d8026ed0c1cc92e8c6298eb29c8510342c
Graham Bell <g.bell@eaobservatory.org>
2015-11-02 16:34:07 -1000

    web: Add decision filters to tabulation page
    There is now a collection of check-boxes for different decision
    states (undecided, accepted and rejected) above the table.
    Unselecting one of the boxes causes the corresponding table rows
    to be hidden.  This works via the decision_xxx classes and an
    additional class "decision_undecided" has been added although
    this currently has no additional styling.
    
    Requested by JTD.

--- data/web/static/js/review_tabulation.js
+++ data/web/static/js/review_tabulation.js
@@ -3,4 +3,23 @@ $(document).ready(function () {
 
     enable_table_sorting(tabulation);
     tabulation.stickyTableHeaders();
+
+    var filter_boxes = $('#filter_decision').find('input[type=checkbox]');
+
+    var filter_table = (function () {
+        filter_boxes.each(function () {
+            var decision = $(this).data('decision');
+            if ($(this).prop('checked')) {
+                tabulation.find('tr.decision_' + decision).show();
+            } else {
+                tabulation.find('tr.decision_' + decision).hide();
+            }
+        });
+    });
+
+    filter_boxes.each(function () {
+        $(this).change(filter_table);
+    });
+
+    filter_table();
 });
