89c7f87caf740673ced762fdb041150ac0859ff5
Graham Bell <g.bell@eaobservatory.org>
2016-01-21 16:44:48 -1000

    test: Add basic unit test of the web app

---
+++ test/dummy_app.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2016 East Asian Observatory
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful,but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,51 Franklin
+# Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+from hedwig import auth
+from hedwig.config import get_config
+from hedwig.web.app import create_web_app
+
+from .dummy_config import DummyConfigTestCase
+from .dummy_db import get_dummy_database
+
+
+class WebAppTestCase(DummyConfigTestCase):
+    facility_spec = 'Generic'
+
+    def setUp(self):
+        DummyConfigTestCase.setUp(self)
+
+        self.config = get_config()
+        self.db = get_dummy_database(facility_spec=self.facility_spec,
+                                     randomize_ids=False)
+        auth._rounds = 10
+        auth.password_hash_delay = 0
+
+        self.app = create_web_app(db=self.db)
+        self.app.config['TESTING'] = True
+
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        DummyConfigTestCase.tearDown(self)
+
+        del self.db
+
+    def log_in(self, user_name, password):
+        return self.client.post(
+            '/user/log_in',
+            data={'user_name': user_name, 'password': password},
+            follow_redirects=True)
+
+    def log_out(self):
+        return self.client.get('/user/log_out', follow_redirects=True)
---
+++ test/test_web_app.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2016 East Asian Observatory
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful,but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,51 Franklin
+# Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+from .dummy_app import WebAppTestCase
+
+
+class BasicWebAppTestCase(WebAppTestCase):
+    def test_home_page(self):
+        rv = self.client.get('/')
+        self.assertEqual(rv.status_code, 200)
+        self.assertEqual(rv.mimetype, 'text/html')
+        self.assertIn(
+            '<h1>' + self.config.get('application', 'name') + '</h1>',
+            rv.data)
+
+    def test_log_in(self):
+        rv = self.log_in('user1', 'pass1')
+        self.assertIn(
+            'User name or password not recognised.',
+            rv.data)
+
+        self.db.add_user('user1', 'pass1')
+
+        rv = self.log_in('user1', 'pass1')
+        self.assertIn(
+            '<li>You have been logged in.</li>',
+            rv.data)
+
+        rv = self.log_out()
+        self.assertIn(
+            '<li>You have been logged out.</li>',
+            rv.data)
