ef6ecb5932f5f1bba73fcaa93966a767b778484f
Graham Bell <g.bell@eaobservatory.org>
2016-06-23 11:20:43 -1000

    test: Add tests of auth module proposal functions

--- test/test_view_auth.py
+++ test/test_view_auth.py
@@ -21,12 +21,26 @@ from __future__ import absolute_import, division, print_function, \
 from contextlib import contextmanager
 from datetime import datetime
 
-from hedwig.type.enum import BaseReviewerRole, FormatType, GroupType
+from hedwig.type.enum import BaseReviewerRole, FormatType, GroupType, \
+    ProposalState
 from hedwig.view import auth
 from hedwig.view.people import _update_session_user, _update_session_person
 from hedwig.web.util import session
 
 from .dummy_app import WebAppTestCase
+
+
+# States for which we will perform tests.  (In vaguely chronological order.)
+proposal_states = [
+    ProposalState.PREPARATION,
+    ProposalState.WITHDRAWN,
+    ProposalState.SUBMITTED,
+    ProposalState.REVIEW,
+    ProposalState.FINAL_REVIEW,
+    ProposalState.ACCEPTED,
+    ProposalState.REJECTED,
+    ProposalState.ABANDONED,
+]
 
 
 class WebAppAuthTestCase(WebAppTestCase):
@@ -136,7 +150,7 @@ class WebAppAuthTestCase(WebAppTestCase):
         # -- technical reviewers
         user_a1rt = self.db.add_user('a1rt', 'pass')
         person_a1rt = self.db.add_person('Person A 1 RT', user_id=user_a1rt)
-        self.db.add_group_member(queue_b, GroupType.TECH, person_a1rt)
+        self.db.add_group_member(queue_a, GroupType.TECH, person_a1rt)
         reviewer_a1rt = self.db.add_reviewer(
             role_class, proposal_a1, person_a1rt, role_class.TECH)
 
@@ -276,6 +290,62 @@ class WebAppAuthTestCase(WebAppTestCase):
                 ]:
             self._test_auth_private_moc(*test_case)
 
+        # Test authorization for proposals and feedback.
+        # Checks authorization in each state via codes -- see _expect_code().
+        for test_case in [
+                # Member access to own proposals.
+                (1,  person_a1e,   False, proposal_a1, 'eeevvvvv', 'ooooovvo'),
+                (2,  person_a2e,   False, proposal_a2, 'eeevvvvv', 'ooooovvo'),
+                (3,  person_a2m,   False, proposal_a2, 'vvvvvvvv', 'ooooovvo'),
+                (4,  person_a2u,   False, proposal_a2, 'vvvvvvvv', 'ooooovvo'),
+                (5,  person_a2u,   False, proposal_a2, 'vvvvvvvv', 'ooooovvo'),
+                # No member access to other proposals.
+                (10, person_a1e,   False, proposal_a2, 'oooooooo', 'oooooooo'),
+                (11, person_a1u,   False, proposal_a2, 'oooooooo', 'oooooooo'),
+                (12, person_a2e,   False, proposal_a1, 'oooooooo', 'oooooooo'),
+                (13, person_a2m,   False, proposal_a1, 'oooooooo', 'oooooooo'),
+                (14, person_a2u,   False, proposal_a1, 'oooooooo', 'oooooooo'),
+                (15, person_a2p,   False, proposal_a1, 'oooooooo', 'oooooooo'),
+                # Admin has access but only when is_admin is set.
+                (20, person_admin, False, proposal_a1, 'oooooooo', 'oooooooo'),
+                (21, person_admin, False, proposal_a2, 'oooooooo', 'oooooooo'),
+                (22, person_admin, True,  proposal_a1, 'vvvvvvvv', 'ooooovvo'),
+                (23, person_admin, True,  proposal_a2, 'vvvvvvvv', 'ooooovvo'),
+                # Review coordinators can see all proposals.
+                (30, person_a_rc,  False, proposal_a1, 'oovvvvvo', 'oooooooo'),
+                (31, person_a_rc,  False, proposal_a2, 'oovvvvvo', 'oooooooo'),
+                (32, person_a_rc,  False, proposal_b1, 'oooooooo', 'oooooooo'),
+                (33, person_a_rc,  False, proposal_b2, 'oooooooo', 'oooooooo'),
+                (34, person_b_rc,  False, proposal_a1, 'oooooooo', 'oooooooo'),
+                (35, person_b_rc,  False, proposal_a2, 'oooooooo', 'oooooooo'),
+                (36, person_b_rc,  False, proposal_b1, 'oovvvvvo', 'oooooooo'),
+                (37, person_b_rc,  False, proposal_b2, 'oovvvvvo', 'oooooooo'),
+                # Committee members can see all proposals.
+                (40, person_a1rc1, False, proposal_a1, 'oovvvvvo', 'oooooooo'),
+                (41, person_a1rc1, False, proposal_a2, 'oovvvvvo', 'oooooooo'),
+                (42, person_a1rc1, False, proposal_b1, 'oooooooo', 'oooooooo'),
+                (43, person_a1rc1, False, proposal_b2, 'oooooooo', 'oooooooo'),
+                (44, person_a1rc2, False, proposal_a1, 'oovvvvvo', 'oooooooo'),
+                (45, person_a1rc2, False, proposal_a2, 'oovvvvvo', 'oooooooo'),
+                (46, person_a1rc2, False, proposal_b1, 'oooooooo', 'oooooooo'),
+                (47, person_a1rc2, False, proposal_b2, 'oooooooo', 'oooooooo'),
+                # Technical reviewers can only see their assigned proposals.
+                (50, person_a1rt,  False, proposal_a1, 'ooovvooo', 'oooooooo'),
+                (51, person_a1rt,  False, proposal_a2, 'oooooooo', 'oooooooo'),
+                (52, person_a1rt,  False, proposal_b1, 'oooooooo', 'oooooooo'),
+                (53, person_a1rt,  False, proposal_b2, 'oooooooo', 'oooooooo'),
+                # External reviewers can only see their assigned proposals.
+                (60, person_a1re,  False, proposal_a1, 'ooovoooo', 'oooooooo'),
+                (61, person_a1re,  False, proposal_a2, 'oooooooo', 'oooooooo'),
+                (62, person_a1re,  False, proposal_b1, 'oooooooo', 'oooooooo'),
+                (63, person_a1re,  False, proposal_b2, 'oooooooo', 'oooooooo'),
+                (64, person_a1reu, False, proposal_a1, 'ooovoooo', 'oooooooo'),
+                (65, person_a1reu, False, proposal_a2, 'oooooooo', 'oooooooo'),
+                (66, person_b1reu, False, proposal_b1, 'ooovoooo', 'oooooooo'),
+                (67, person_b1reu, False, proposal_b2, 'oooooooo', 'oooooooo'),
+                ]:
+            self._test_auth_proposal(role_class, *test_case)
+
     def _test_auth_call_review(self, case_number, person_id, is_admin,
                                call_id, expect):
         call = self.db.get_call(None, call_id)
@@ -307,6 +377,42 @@ class WebAppAuthTestCase(WebAppTestCase):
             self.assertTrue(can, 'auth private moc {}'.format(case_number))
         else:
             self.assertFalse(can, 'auth private moc {}'.format(case_number))
+
+    def _test_auth_proposal(self, role_class, case_number, person_id, is_admin,
+                            proposal_id, expect_codes, expect_codes_fb):
+        self.assertEqual(len(expect_codes), len(proposal_states),
+                         'codes for proposal case {}'.format(case_number))
+        self.assertEqual(len(expect_codes_fb), len(proposal_states),
+                         'fb codes for proposal case {}'.format(case_number))
+
+        for (state, expect_code, expect_code_fb) in zip(
+                proposal_states, expect_codes, expect_codes_fb):
+            expect = self._expect_code('proposal', case_number, expect_code)
+            expect_fb = self._expect_code('proposal fb', case_number,
+                                          expect_code_fb)
+            self.db.update_proposal(proposal_id, state=state)
+            proposal = self.db.get_proposal(
+                None, proposal_id, with_members=True, with_reviewers=True)
+            with self._as_person(person_id, is_admin):
+                self.assertEqual(
+                    auth.for_proposal(role_class, self.db, proposal),
+                    expect, 'auth proposal case {} state {}'.format(
+                        case_number, ProposalState.get_name(state)))
+                self.assertEqual(
+                    auth.for_proposal_feedback(role_class, self.db, proposal),
+                    expect_fb, 'auth proposal fb case {} state {}'.format(
+                        case_number, ProposalState.get_name(state)))
+
+    def _expect_code(self, case_type, case_number, code):
+        if code == 'e':
+            return auth.yes
+        elif code == 'v':
+            return auth.view_only
+        elif code == 'o':
+            return auth.no
+        else:
+            self.fail('invalid code for {} case {}'.format(
+                case_type, case_number))
 
     @contextmanager
     def _as_person(self, person_id, is_admin):
