8e3e8113d00e8978247a98a434497b282282c571
Graham Bell <g.bell@eaobservatory.org>
2016-06-22 14:23:12 -1000

    test: Add more tests for the auth module

--- test/test_view_auth.py
+++ test/test_view_auth.py
@@ -40,6 +40,8 @@ class WebAppAuthTestCase(WebAppTestCase):
 
         # Set up test database entries.
         facility_id = self.db.ensure_facility('test')
+        facility_other = self.db.ensure_facility('other')
+
         semester_id = self.db.add_semester(
             facility_id, 'Test', 'T',
             datetime(2000, 1, 1), datetime(2000, 7, 1))
@@ -57,6 +59,10 @@ class WebAppAuthTestCase(WebAppTestCase):
         call_a = self.db.add_call(semester_id, queue_a, *call_options)
         call_b = self.db.add_call(semester_id, queue_b, *call_options)
 
+        institution_1 = self.db.add_institution('Inst 1', '', '', '', 'AX')
+        institution_2 = self.db.add_institution('Inst 2', '', '', '', 'AX')
+        institution_3 = self.db.add_institution('Inst 3', '', '', '', 'AX')
+
         user_admin = self.db.add_user('admin', 'pass')
         person_admin = self.db.add_person('Admin', user_id=user_admin)
         self.db.update_person(person_admin, admin=True)
@@ -66,9 +72,14 @@ class WebAppAuthTestCase(WebAppTestCase):
         # Where <info> is: Editor / Member / Unregistered / Public
         user_a1e = self.db.add_user('a1e', 'pass')
         person_a1e = self.db.add_person('Person A 1 E', user_id=user_a1e)
+        self.db.update_person(person_a1e, institution_id=institution_1)
+
+        person_a1u = self.db.add_person('Person A 1 U', user_id=None)
+        self.db.update_person(person_a1u, institution_id=institution_2)
 
         user_a2e = self.db.add_user('a2e', 'pass')
         person_a2e = self.db.add_person('Person A 2 E', user_id=user_a2e)
+        self.db.update_person(person_a2e, institution_id=institution_2)
 
         user_a2m = self.db.add_user('a2r', 'pass')
         person_a2m = self.db.add_person('Person A 2 R', user_id=user_a2m)
@@ -82,11 +93,18 @@ class WebAppAuthTestCase(WebAppTestCase):
         user_b1e = self.db.add_user('b1e', 'pass')
         person_b1e = self.db.add_person('Person B 1 E', user_id=user_b1e)
 
+        person_b1u = self.db.add_person('Person B 1 U', user_id=None)
+        self.db.update_person(person_b1u, institution_id=institution_3)
+
         user_b2e = self.db.add_user('b2e', 'pass')
         person_b2e = self.db.add_person('Person B 2 E', user_id=user_b2e)
 
+        person_b2u = self.db.add_person('Person B 2 U', user_id=None)
+        self.db.update_person(person_b2u, institution_id=institution_3)
+
         proposal_a1 = self.db.add_proposal(
             call_a, person_a1e, affiliation_a, 'Test proposal A 1')
+        self.db.add_member(proposal_a1, person_a1u, affiliation_a)
 
         proposal_a2 = self.db.add_proposal(
             call_a, person_a2e, affiliation_a, 'Test proposal A 2')
@@ -96,9 +114,11 @@ class WebAppAuthTestCase(WebAppTestCase):
 
         proposal_b1 = self.db.add_proposal(
             call_b, person_b1e, affiliation_b, 'Test proposal B 1')
+        self.db.add_member(proposal_b1, person_b1u, affiliation_b)
 
         proposal_b2 = self.db.add_proposal(
             call_b, person_b2e, affiliation_b, 'Test proposal B 2')
+        self.db.add_member(proposal_b2, person_b2u, affiliation_b)
 
         # Create review coordinator profiles.
         user_a_rc = self.db.add_user('arc', 'pass')
@@ -111,8 +131,16 @@ class WebAppAuthTestCase(WebAppTestCase):
 
         # Create reviews and reviewer profiles.
         # Naming convention: <info> = r<type><options>
-        # Where <type> is: External
+        # Where <type> is: External / Technical / Committee
         # And <options> are: Unregistered
+        # -- technical reviewers
+        user_a1rt = self.db.add_user('a1rt', 'pass')
+        person_a1rt = self.db.add_person('Person A 1 RT', user_id=user_a1rt)
+        self.db.add_group_member(queue_b, GroupType.TECH, person_a1rt)
+        reviewer_a1rt = self.db.add_reviewer(
+            role_class, proposal_a1, person_a1rt, role_class.TECH)
+
+        # -- external reviewers:
         user_a1re = self.db.add_user('a1re', 'pass')
         person_a1re = self.db.add_person('Person A 1 RE', user_id=user_a1re)
         reviewer_a1re = self.db.add_reviewer(
@@ -125,6 +153,42 @@ class WebAppAuthTestCase(WebAppTestCase):
         person_b1reu = self.db.add_person('Person B 1 REU', user_id=None)
         reviewer_b1reu = self.db.add_reviewer(
             role_class, proposal_b1, person_b1reu, role_class.EXTERNAL)
+
+        # -- committee reviwers:
+        user_a1rc1 = self.db.add_user('a1rc1', 'pass')
+        person_a1rc1 = self.db.add_person('Person A 1 RC1', user_id=user_a1rc1)
+        self.db.add_group_member(queue_a, GroupType.CTTEE, person_a1rc1)
+        reviewer_a1rc1 = self.db.add_reviewer(
+            role_class, proposal_a1, person_a1rc1, role_class.CTTEE_PRIMARY)
+
+        user_a1rc2 = self.db.add_user('a1rc2', 'pass')
+        person_a1rc2 = self.db.add_person('Person A 1 RC2', user_id=user_a1rc2)
+        self.db.add_group_member(queue_a, GroupType.CTTEE, person_a1rc2)
+        reviewer_a1rc2 = self.db.add_reviewer(
+            role_class, proposal_a1, person_a1rc2, role_class.CTTEE_SECONDARY)
+
+        # Test authorization for call reviews.
+        for test_case in [
+                # No general access to call reviews.
+                (1,  person_a1e,   False, call_a, auth.no),
+                (2,  person_a1e,   False, call_b, auth.no),
+                # Admin has full access but only when is_admin is set.
+                (3,  person_admin, False, call_a, auth.no),
+                (4,  person_admin, False, call_b, auth.no),
+                (5,  person_admin, True,  call_a, auth.yes),
+                (6,  person_admin, True,  call_b, auth.yes),
+                # Review coordinators have full access to corresponding call.
+                (7,  person_a_rc,  False, call_a, auth.yes),
+                (8,  person_a_rc,  False, call_b, auth.no),
+                (9,  person_b_rc,  False, call_a, auth.no),
+                (10, person_b_rc,  False, call_b, auth.yes),
+                # Committee members have view access to corresponding call.
+                (11, person_a1rc1, False, call_a, auth.view_only),
+                (12, person_a1rc2, False, call_a, auth.view_only),
+                (13, person_a1rc1, False, call_b, auth.no),
+                (14, person_a1rc2, False, call_b, auth.no),
+                ]:
+            self._test_auth_call_review(*test_case)
 
         # Test authorization for person profiles.
         for test_case in [
@@ -172,12 +236,77 @@ class WebAppAuthTestCase(WebAppTestCase):
                 ]:
             self._test_auth_person(*test_case)
 
+        # Test authorization for institution profiles.
+        for test_case in [
+                # Allow full access to own institution.
+                (1,  person_a1e,   False, institution_1, auth.yes),
+                (2,  person_a1e,   False, institution_2, auth.view_only),
+                (3,  person_a2e,   False, institution_1, auth.view_only),
+                (4,  person_a2e,   False, institution_2, auth.yes),
+                # Admin has full access but only when is_admin is set.
+                (5,  person_admin, False, institution_1, auth.view_only),
+                (6,  person_admin, False, institution_2, auth.view_only),
+                (7,  person_admin, True,  institution_1, auth.yes),
+                (8,  person_admin, True,  institution_2, auth.yes),
+                # Proposal editor has full access to a co-member's institution
+                # if it has no registered members (only Institution 3).
+                (9,  person_a1e,   False, institution_2, auth.view_only),
+                (10, person_a1e,   False, institution_3, auth.view_only),
+                (11, person_a2e,   False, institution_3, auth.view_only),
+                (12, person_b1e,   False, institution_3, auth.yes),
+                (13, person_b2e,   False, institution_3, auth.yes),
+                ]:
+            self._test_auth_institution(*test_case)
+
+        # Test authorization for private MOCs.
+        for test_case in [
+                # No general access.
+                (1,  person_a1e,   False, facility_id,    False),
+                (2,  person_a1e,   False, facility_id,    False),
+                (3,  person_a1e,   False, facility_other, False),
+                (4,  person_a1e,   False, facility_other, False),
+                # Admin has access but only when is_admin is set.
+                (5,  person_admin, False, facility_id,    False),
+                (6,  person_admin, False, facility_other, False),
+                (7,  person_admin, True,  facility_id,    True),
+                (8,  person_admin, True,  facility_other, True),
+                # Tech. assessors have access to the corresponding facility.
+                (9,  person_a1rt,  False, facility_id,    True),
+                (10, person_a1rt,  False, facility_other, False),
+                ]:
+            self._test_auth_private_moc(*test_case)
+
+    def _test_auth_call_review(self, case_number, person_id, is_admin,
+                               call_id, expect):
+        call = self.db.get_call(None, call_id)
+        with self._as_person(person_id, is_admin):
+            self.assertEqual(
+                auth.for_call_review(self.db, call),
+                expect, 'auth call review case {}'.format(case_number))
+
     def _test_auth_person(self, case_number, person_id, is_admin,
                           for_person_id, expect):
         with self._as_person(person_id, is_admin):
             self.assertEqual(
                 auth.for_person(self.db, self.db.get_person(for_person_id)),
                 expect, 'auth person case {}'.format(case_number))
+
+    def _test_auth_institution(self, case_number, person_id, is_admin,
+                               institution_id, expect):
+        institution = self.db.get_institution(institution_id)
+        with self._as_person(person_id, is_admin):
+            self.assertEqual(
+                auth.for_institution(self.db, institution),
+                expect, 'auth institution case {}'.format(case_number))
+
+    def _test_auth_private_moc(self, case_number, person_id, is_admin,
+                               facility_id, expect):
+        with self._as_person(person_id, is_admin):
+            can = auth.for_private_moc(self.db, facility_id)
+        if expect:
+            self.assertTrue(can, 'auth private moc {}'.format(case_number))
+        else:
+            self.assertFalse(can, 'auth private moc {}'.format(case_number))
 
     @contextmanager
     def _as_person(self, person_id, is_admin):
