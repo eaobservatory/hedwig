6331f714f5f4655ace08ca50681bbb727491a4a2
Graham Bell <g.bell@eaobservatory.org>
2019-07-15 13:57:37 -1000

    web: Add assignment totals to reviewer grid
    This adds new table cells containing a span element to contain
    the total number of assignments for each person and for
    each proposal (and role).  These totals are computed by the
    JavaScript routine for this page, but when the page is initially
    loaded and whenever the selection is changed.

--- data/web/static/js/reviewer_grid.js
+++ data/web/static/js/reviewer_grid.js
@@ -3,25 +3,70 @@ $(document).ready(function () {
     table.stickyTableHeaders();
 
     var nroles = table.data('nroles');
-    if (! (nroles > 1)) {
-        return;
-    }
 
     var assign_radio = table.find('input[type=radio]');
     var assign_check = table.find('input[type=checkbox]');
+    var assign_hidden = table.find('input[type=hidden]');
 
-    var disable_duplicate_control = (function () {
-        var control = $(this);
-        var other = control.siblings('input');
-        control.prop('disabled', other.is(':checked') || other.is('[type=hidden]'));
-    });
+    if (nroles > 1) {
+        var disable_duplicate_control = (function () {
+            var control = $(this);
+            var other = control.siblings('input');
+            control.prop('disabled', other.is(':checked') || other.is('[type=hidden]'));
+        });
 
-    var disable_duplicates = (function () {
-        assign_radio.not('.unassigned').each(disable_duplicate_control);
-        assign_check.not('.unassigned').each(disable_duplicate_control);
-    });
+        var disable_duplicates = (function () {
+            assign_radio.not('.unassigned').each(disable_duplicate_control);
+            assign_check.not('.unassigned').each(disable_duplicate_control);
+        });
 
-    assign_radio.change(disable_duplicates);
-    assign_check.change(disable_duplicates);
-    disable_duplicates();
+        assign_radio.change(disable_duplicates);
+        assign_check.change(disable_duplicates);
+        disable_duplicates();
+    }
+
+    var total_person = table.find('span[id^="total_person_"]');
+    var total_proposal = table.find('span[id^="total_proposal_"]');
+
+    if (0 < (total_person.length + total_proposal.length)) {
+        var compute_totals = (function () {
+            var re_unique = new RegExp('^rev_\\d+_\\d+$');
+            total_person.each(function () {
+                var span = $(this);
+                var person_id = span.attr('id').replace('total_person_', '');
+                var total = assign_check.filter(':checked').filter('[name$="_' + person_id + '"]').length;
+                total += assign_radio.filter(':checked').filter('[value="' + person_id + '"]').length;
+
+                var re_non_unique = new RegExp('^rev_\\d+_\\d+_' + person_id + '$');
+                assign_hidden.each(function() {
+                    var hidden = $(this);
+                    var name = hidden.attr('name');
+                    if (name.match(re_unique)) {
+                        if (hidden.val() === person_id) {
+                            total += 1;
+                        }
+                    } else if (name.match(re_non_unique)) {
+                        total += 1;
+                    }
+                });
+
+                span.html(total);
+            });
+
+
+            total_proposal.each(function () {
+                var span = $(this);
+                var role_prop_id = span.attr('id').replace('total_proposal_', '');
+                var total = assign_check.filter(':checked').filter('[name^="rev_' + role_prop_id + '_"]').length;
+                total += assign_radio.filter(':checked').filter('[name="rev_' + role_prop_id + '"]').not('[value=""]').length;
+                total += assign_hidden.filter('[name^="rev_' + role_prop_id + '_"]').length;
+                total += assign_hidden.filter('[name="rev_' + role_prop_id + '"]').length;
+                span.html(total);
+            });
+        });
+
+        assign_radio.change(compute_totals);
+        assign_check.change(compute_totals);
+        compute_totals();
+    }
 });
