d8ef9a3c7a5a46ad7803b4dd2e93994a8832950d
Graham Bell <g.bell@eaobservatory.org>
2019-08-06 11:01:41 -1000

    web: Enable table sorting reviewer statistics page
    Support for sorting by column was added to the "hedwig_util.js"
    library and the associated CSS rules.

--- data/web/static/js/hedwig_util.js
+++ data/web/static/js/hedwig_util.js
@@ -66,3 +66,83 @@ function enable_table_sorting(table) {
         enable_sort_column($(this));
     });
 }
+
+function enable_table_col_sorting(table) {
+    var sort_headings = table.find('th.col_sortable');
+    var last_sorted_by = null;
+    var sort_direction = -1;
+
+    var enable_sort_column = (function (heading) {
+        var sort_key = heading.data('col_sortkey');
+        var default_sort_order = 1;
+
+        if (heading.hasClass('col_sortreverse')) {
+            default_sort_order = -1;
+        }
+
+        if (heading.hasClass('col_sortedalready')) {
+            last_sorted_by = sort_key;
+        }
+
+        heading.click(function () {
+            if (sort_key !== last_sorted_by) {
+                last_sorted_by = sort_key;
+                sort_direction = default_sort_order;
+            }
+
+            var arr = [];
+            var unsorted = [];
+            var i = 0;
+
+            table.find('tr').first().children().each(function (i) {
+                var col = $(this);
+                var sort_info = col.data('col_sortinfo');
+
+                if (sort_info !== undefined) {
+                    arr.push({col: i, value: sort_info[sort_key]});
+                } else if (arr.length == 0) {
+                    unsorted.push(i);
+                }
+            });
+
+            arr.sort(function (a, b) {
+                if (a.value === b.value) {
+                    return 0;
+                }
+                if (a.value === null && b.value !== null) {
+                    return 1;
+                }
+                if (b.value === null && a.value !== null) {
+                    return -1;
+                }
+                return (sort_direction * (a.value < b.value ? -1 : 1));
+            });
+
+            table.find('tr').each(function () {
+                var row = $(this);
+                var cells = row.children();
+                var sorted = [];
+
+                for (i in unsorted) {
+                    sorted.push(cells.eq(unsorted[i]).detach());
+                }
+
+                for (i in arr) {
+                    sorted.push(cells.eq(arr[i].col).detach());
+                }
+
+                row.prepend(sorted);
+            });
+
+            sort_direction = - sort_direction;
+
+            sort_headings.removeClass('col_sorted_asc').addClass('col_sortable');
+            sort_headings.removeClass('col_sorted_desc').addClass('col_sortable');
+            heading.removeClass('col_sortable').addClass((sort_direction < 0 ) ? 'col_sorted_asc' : 'col_sorted_desc');
+        });
+    });
+
+    sort_headings.each(function () {
+        enable_sort_column($(this));
+    });
+}
--- data/web/static/js/review_statistics.js
+++ data/web/static/js/review_statistics.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
     var table = $('table#statistics');
     table.stickyTableHeaders();
+
+    enable_table_sorting(table);
+    enable_table_col_sorting(table);
 });
