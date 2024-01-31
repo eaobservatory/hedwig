14afab74987cac966a38809fbca2e084cab23bef
Graham Bell <g.bell@eaobservatory.org>
2024-01-30 14:05:33 -1000

    jcmt: Match end when parsing proposal code

--- lib/hedwig/facility/jcmt/view.py
+++ lib/hedwig/facility/jcmt/view.py
@@ -189,7 +189,9 @@ class JCMT(EAOFacility):
         type_class = self.get_call_types()
 
         try:
-            m = re.match(r'([A-Z])(\d\d[ABXYZW])([A-Z])(\d\d\d)', proposal_code)
+            m = re.match(
+                r'([A-Z])(\d\d[ABXYZW])([A-Z])(\d\d\d)$',
+                proposal_code)
 
             if not m:
                 raise ParseError(
