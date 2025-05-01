928c18aef31027ac24595979fc6c498f4787e7f3
Graham Bell <g.bell@eaobservatory.org>
2025-05-01 09:50:32 -1000

    file: Remove ghostscript option -dNOTRANSPARENCY
    This can cause incorrect processing of PDFs with transparent
    elements.  Supposedly the use of the "png16m" device should
    be sufficient to avoid transparent pixels in the output.

--- lib/hedwig/file/pdf.py
+++ lib/hedwig/file/pdf.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2024 East Asian Observatory
+# Copyright (C) 2015-2025 East Asian Observatory
 # All Rights Reserved.
 #
 # This program is free software; you can redistribute it and/or modify it under
@@ -148,7 +148,6 @@ def _pdf_ps_to_png(buff, page_count, resolution=100, downscale=4):
         '-dBATCH',
         '-dSAFER',
         '-dPARANOIDSAFER',
-        '-dNOTRANSPARENCY',
         '-sDEVICE=png16m',
         '-r{}'.format(resolution * downscale),
         '-dGraphicsAlphaBits=4',
